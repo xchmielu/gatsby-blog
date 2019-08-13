6cf61c474a0e47a2d3a253959aba6e3bbb05de9e
xchmielu <mchmielewski989@gmail.com>
2019-08-13 21:35:47 +0200

    fix alts

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default () => (
     <div>
         <h1>Page not found!</h1>
-        <img src="pluto-page-not-found.png" />
+        <img alt="404" src="pluto-page-not-found.png" />
     </div>
     
 
