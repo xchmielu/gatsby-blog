958ce902d9523248576160e0a2f55e5858d3fe18
xchmielu <mchmielewski989@gmail.com>
2019-09-13 20:23:20 +0200

    edited content v2

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export default () => (
         <h1 className="about--h1">About</h1>
         <div className="about">
         <img className="about--image" src="pluto-welcome.png" alt=""/>
-        <p className="about--lead">Hello, I am 3rd year student of Warsaw School of Computer Science in specialization of Databases. !</p>
+        <p className="about--lead">Hello, I am 3rd year student of Warsaw School of Computer Science in specialization of Databases. My passion is to create and build software using JavaScript libraries. My free time mostly spend on learning new techs. In this time I'am learnign TypeScript and preapring it to use with <strong>Koa.js</  strong>.</p>
         </div>
         <h2 className="about--h2">Skills</h2>
         <div className="about__skills">
