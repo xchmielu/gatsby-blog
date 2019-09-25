8b9169fa2d3e40d07ff6fcece416a2c7c8c0348c
xchmielu <mchmielewski989@gmail.com>
2019-09-25 17:40:12 +0200

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import PrimaryLayout from '../components/layouts/PrimaryLayout'
 
 export default () => (
+    <PrimaryLayout>
     <div className="notfound">
+        
         <h1>Page not found!</h1>
         <img alt="404" src="https://s3.eu-central-1.amazonaws.com/chmielewski.dev/pluto-page-not-found.webp" />
+
     </div>
-    
+    </PrimaryLayout>
 
 
 )
